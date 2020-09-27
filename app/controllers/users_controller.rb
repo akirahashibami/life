f469160576b53e562102707bb48f6c6f750a129d
akirahashibami <hashibami2207@gmail.com>
2020-09-27 08:04:43 +0000

    [add] image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,16 +12,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
-  # def update
-  #   @user = User.find(params[:id])
-  #   if @user.update(user_params)
-  #     redirect_to user_path(@user)
-  #   else
-  #     render :edit
-  #   end
-  # end
-
+  
   def update
     @user = User.find(params[:id])
     case !params[:user][:name].blank?
